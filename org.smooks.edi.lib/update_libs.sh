4fcd6c5b12f6b04f596453908db78100ad04fe20
Renat Zubairov <renat.zubairov@gmail.com>
2011-04-28 16:23:39 +0200

    Updated script to update libraries

--- org.smooks.edi.lib/update_libs.sh
+++ org.smooks.edi.lib/update_libs.sh
@@ -7,3 +7,5 @@ cp ~/.m2/repository/org/milyn/milyn-edisax-parser/${VERSION}/milyn-edisax-parser
 cp ~/.m2/repository/org/milyn/milyn-smooks-core/${VERSION}/milyn-smooks-core-${VERSION}.jar lib
 cp ~/.m2/repository/org/milyn/milyn-smooks-ect/${VERSION}/milyn-smooks-ect-${VERSION}.jar lib
 cp ~/.m2/repository/org/milyn/milyn-smooks-javabean/${VERSION}/milyn-smooks-javabean-${VERSION}.jar lib
+git add lib
+git status
